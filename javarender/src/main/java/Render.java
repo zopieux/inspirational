ef03fe78605a1ff9e57dac1a9bd423321a681485
Alexandre Macabies <web+oss@zopieux.com>
2018-05-01 04:00:00 +0200

    Minor Java cleanup

--- javarender/src/main/java/Render.java
+++ javarender/src/main/java/Render.java
@@ -5,8 +5,6 @@ import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
@@ -16,10 +14,69 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Render {
-
     private static final int Y_MARGIN = 16;
 
-    private static TextLayout fitWidth(FontRenderContext frc, final String text, final Font font, final int width) {
+    public static void main(String[] args) throws IOException, FontFormatException, ParseException {
+        System.setProperty("java.awt.headless", "true");
+        assert GraphicsEnvironment.isHeadless();
+
+        Options options = new Options();
+        options.addOption(Option.builder("w").longOpt("width").hasArg().required().desc("Width").type(Number.class).build());
+        options.addOption(Option.builder("h").longOpt("height").hasArg().required().desc("Height").type(Number.class).build());
+        options.addOption(Option.builder("q").longOpt("quality").hasArg().required().desc("Quality").type(Number.class).build());
+        options.addOption(Option.builder("f").longOpt("font").hasArg().required().desc("Font").build());
+        options.addOption(Option.builder("i").longOpt("input").hasArg().required().desc("Background image").build());
+        options.addOption(Option.builder("t").longOpt("text").hasArg().required().desc("Text").build());
+
+        CommandLine cmdline = new DefaultParser().parse(options, args);
+
+        final int width = ((Number) cmdline.getParsedOptionValue("w")).intValue();
+        final int height = ((Number) cmdline.getParsedOptionValue("h")).intValue();
+        final float quality = ((Number) cmdline.getParsedOptionValue("q")).floatValue();
+        final String fontName = cmdline.getOptionValue("f");
+        final String inputImage = cmdline.getOptionValue("i");
+        final String text = cmdline.getOptionValue("t");
+
+        Canvas canvas = new Canvas();
+        canvas.setSize(width, height);
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        g.setBackground(Color.WHITE);
+        g.setRenderingHints(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
+
+        BufferedImage bgRaw = ImageIO.read(new File(inputImage));
+        // TODO: this is not equivalent to Qt KeepAspectRatioByExpanding
+        g.drawImage(Scalr.resize(bgRaw, Scalr.Method.QUALITY, Scalr.Mode.FIT_TO_WIDTH, width, height), 0, 0, null);
+
+        FontRenderContext frc = g.getFontMetrics().getFontRenderContext();
+        Font inputFont = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File(fontName)));
+
+        final int x = width / 2;
+        int y = 128;
+
+        for (String line : text.split("\n")) {
+            TextLayout layout = fitTextInWidth(frc, line, inputFont, width / 3);
+            Rectangle bounds = layout.getPixelBounds(frc, 0, 0);
+            drawStringShadow(layout, g, -bounds.x + x - bounds.width / 2, -bounds.y + y, 0, 3);
+            y += bounds.height + Y_MARGIN;
+        }
+
+        // JPEG writer to stdout
+        ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+        writer.setOutput(ImageIO.createImageOutputStream(System.out));
+
+        // adjust JPEG quality
+        ImageWriteParam params = writer.getDefaultWriteParam();
+        params.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        params.setCompressionQuality(quality);
+
+        writer.write(null, new IIOImage(image, null, null), params);
+        writer.dispose();
+    }
+
+    private static TextLayout fitTextInWidth(FontRenderContext frc, final String text, final Font font, final int width) {
         TextLayout layout = null;
         float min = 1f, max = 1000f;
         while (max - min > 1f) {
@@ -37,69 +94,7 @@ public class Render {
         return layout;
     }
 
-    public static void main(String[] args) throws IOException, FontFormatException, ParseException {
-        System.setProperty("java.awt.headless", "true");
-        assert GraphicsEnvironment.isHeadless();
-
-        Options options = new Options();
-        options.addOption(Option.builder("w").longOpt("width").hasArg().required().desc("Width").type(Number.class).build());
-        options.addOption(Option.builder("h").longOpt("height").hasArg().required().desc("Height").type(Number.class).build());
-        options.addOption(Option.builder("q").longOpt("quality").hasArg().required().desc("Quality").type(Number.class).build());
-        options.addOption(Option.builder("f").longOpt("font").hasArg().required().desc("Font").build());
-        options.addOption(Option.builder("i").longOpt("input").hasArg().required().desc("Input").build());
-        options.addOption(Option.builder("t").longOpt("text").hasArg().required().desc("Text").build());
-
-        CommandLine cmdline = new DefaultParser().parse(options, args);
-
-        final float quality = ((Number) cmdline.getParsedOptionValue("q")).floatValue();
-
-        System.err.println(System.currentTimeMillis() + " parse");
-        Canvas canvas = new Canvas();
-        canvas.setSize(
-                ((Number) cmdline.getParsedOptionValue("w")).intValue(),
-                ((Number) cmdline.getParsedOptionValue("h")).intValue());
-
-        BufferedImage image = new BufferedImage(canvas.getWidth(), canvas.getHeight(), BufferedImage.TYPE_INT_RGB);
-
-        Graphics2D g = (Graphics2D) image.getGraphics();
-        g.setBackground(Color.WHITE);
-        g.setRenderingHints(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
-        g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        g.setColor(Color.BLACK);
-        System.err.println(System.currentTimeMillis() + " canvasImageGraphics");
-
-        FontRenderContext frc = g.getFontMetrics().getFontRenderContext();
-        Font inputFont = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File(cmdline.getOptionValue("f"))));
-        System.err.println(System.currentTimeMillis() + " fontLoaded");
-
-        BufferedImage bgRaw = ImageIO.read(new File(cmdline.getOptionValue("i")));
-        BufferedImage bg = Scalr.resize(bgRaw, Scalr.Method.QUALITY, Scalr.Mode.AUTOMATIC, canvas.getWidth(), canvas.getHeight());
-        g.drawImage(bg, 0, 0, null);
-        System.err.println(System.currentTimeMillis() + " bgLoaded");
-
-        final int x = canvas.getWidth() / 2;
-        int y = 128;
-
-        for (String line : cmdline.getOptionValue("t").split("\n")) {
-            TextLayout layout = fitWidth(frc, line, inputFont, canvas.getWidth() / 3);
-            Rectangle bounds = layout.getPixelBounds(frc, 0, 0);
-            drawShadow(layout, g, -bounds.x + x - bounds.width / 2, -bounds.y + y, 0, 3);
-            y += bounds.height + Y_MARGIN;
-        }
-        System.err.println(System.currentTimeMillis() + " textDrawn");
-
-        ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
-        writer.setOutput(ImageIO.createImageOutputStream(System.out));
-        ImageWriteParam params = writer.getDefaultWriteParam();
-        params.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        params.setCompressionQuality(quality);
-        IIOImage outputImage = new IIOImage(image, null, null);
-        writer.write(null, outputImage, params);
-        writer.dispose();
-        System.err.println(System.currentTimeMillis() + " image wrote");
-    }
-
-    private static void drawShadow(TextLayout layout, Graphics2D g, int x, int y, int dx, int dy) {
+    private static void drawStringShadow(TextLayout layout, Graphics2D g, int x, int y, int dx, int dy) {
         g.setColor(new Color(0, 0, 0, .5f));
         layout.draw(g, x + dx, y + dy);
         g.setColor(Color.WHITE);
